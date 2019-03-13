9126e9b6c47c5a0464773d8d03a72ceeacea188a
David Dropping <ddroppin@mail.sfsu.edu>
2019-03-13 11:26:17 -0700

    read user input to buffer, tokenize substrings from buffer, fill myargc and myargv, print myargv and myargc

--- myshell.c
+++ myshell.c
@@ -31,17 +31,43 @@ main(int* argc, char** argv)
 {
 
     char buffer[BUFFERSIZE];
+    int myargc; //number of strings encountered
+    char *myargv[100]; //collection of sub-strings
 
 
-    printf("shell: ");
-    //memset(starting value of mem, value to be filled, number of bytes to be filled)
+
+    printf("%s", PROMPT);
+//read user input into buffer, fill rest of buffer with \0
     memset(buffer, '\0',BUFFERSIZE );
     fgets(buffer, BUFFERSIZE, stdin);
 
-//    print buffer
-//    for ( int i = 0; i < BUFFERSIZE; i++ ){
-//        putc( isprint(buffer[i]) ? buffer[i] : '.' , stdout );
-//    }
+//print contents of buffer to console
+    printf("\n");
+    for ( int i = 0; i < BUFFERSIZE; i++ ){
+        putc( isprint(buffer[i]) ? buffer[i] : '.' , stdout );
+    }
+    printf("\n");
+
+//tokenize the substrings
+    char* token = strtok(buffer, " ");
+    int tokenNum=0;
+    while(token != NULL){
+        myargv[tokenNum] = token;
+        token = strtok(NULL, " ");
+        tokenNum++;
+    }
+
+//set myargc
+    myargc=tokenNum;
+
+//prints contents of myargv and myargc
+    printf("\n");
+    printf("myargc: %d\n", myargc);
+    for(int i=0; i< 100; i++){
+            printf("myargv[%d]: %s\n",i, myargv[i]);
+    }
+    printf("myargc: %d", myargc);
+    printf("\n");
 
 
 return 0;
