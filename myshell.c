67ae05c9574a848dc8aada67364d3ad2325fe752
ddropping <ddropping@gmail.com>
2019-03-17 22:55:59 -0700

    refactored code: POSSIBLE ERROR

--- myshell.c
+++ myshell.c
@@ -19,7 +19,7 @@
 
 #include <stdbool.h>
 
-/* CANNOT BE CHANGED */
+ /* CANNOT BE CHANGED */
 #define BUFFERSIZE 256
 /* --------------------*/
 #define PROMPT "myShell >> "
@@ -30,241 +30,172 @@ int
 main(int* argc, char** argv)
 {
 
-    char buffer[BUFFERSIZE];
-    int myargc; //number of strings encountered
+	char buffer[BUFFERSIZE];
+	int myargc; //number of strings encountered
 
-while (true) {
+	while (true) {
 
-    char *myargv[100] = {NULL}; //collection of sub-strings
+		char *myargv[100] = { NULL }; //collection of sub-strings
 
-    /*===========================================================================================
-    1.Implement your shell to simply initialize your shell, display a prompt, read in user
-    input and print it back to the console.
-    ===========================================================================================*/
+		printf("%s", PROMPT);
 
-        printf("%s", PROMPT);
+		//read user input into buffer, fill rest of buffer with \0
+		memset(buffer, '\0', BUFFERSIZE);
+		fgets(buffer, BUFFERSIZE, stdin);
 
-    //read user input into buffer, fill rest of buffer with \0
-        memset(buffer, '\0',BUFFERSIZE );
-        fgets(buffer, BUFFERSIZE, stdin);
+		//tokenize the substrings
+		char* token = strtok(buffer, " \n");
+		int tokenNum = 0;
+		while (token != NULL) {
+			myargv[tokenNum] = token;
+			token = strtok(NULL, " \n");
+			tokenNum++;
+		}
 
+		//set myargc
+		myargc = tokenNum;
 
-    /*===========================================================================================
-    2.Add functionality to your shell to parse user input setting the correct values for myargv
-    and myargc. Once parsed, print myargv and myargc to the console. Note that myargvs
-    need to be null terminated for the exec commands to interpret them correctly
-    ===========================================================================================*/
-    //note: may need to add string terminator to end of each substring in myargv...
-
-    //tokenize the substrings
-        char* token = strtok(buffer, " \n");
-        int tokenNum=0;
-        while(token != NULL){
-            myargv[tokenNum] = token;
-            token = strtok(NULL, " \n");
-            tokenNum++;
-        }
-
-    //set myargc
-        myargc=tokenNum;
-
-    // //prints contents of myargv and myargc
-        // printf("\n");
-        // printf("myargc: %d\n", myargc);
-        // int i=0;
-        // while(myargv[i]){
-        //     printf("myargv[%d]: %s\n", i, myargv[i]);
-        //     i++;
-        // }
-        // printf("\n");
-
-    /*===========================================================================================
-    3.Add functionality to your shell to execute simple shell commands. Start with commands like
-    ls, then commands with options like ls -la /home.
-    ===========================================================================================*/
-    //code at bottom of program
-
-    //break if user enters 'exit'
-        if(strcmp(myargv[0], "exit") == 0) {
+		//break if user enters 'exit'
+		if (strcmp(myargv[0], "exit") == 0) {
 			return 0;
 		}
 
 
-    /*===========================================================================================
-    4.Add functionality to shell to execute input and output redirection. It is required to
-    implement >, >>, and <.
-    ===========================================================================================*/
-        
-        
-        pid_t id;
-        id=fork();
-        if(id == 0){
-            int fd0, fd1, fd2;
-            char input[64], output[64], append[64];
-            bool in=false, out=false, app=false;
-
-    //determine if statement is redirection
-            for(int i=0; i< myargc; i++){
-                if(strcmp(myargv[i], "<")==0){
-                    in = true;
-                    myargv[i] = NULL;
-                    strcpy(input, myargv[i+1]);
-                }
-                else if(strcmp(myargv[i], ">")==0){
-                    out = true;
-                    myargv[i] = NULL;
-                    strcpy(output, myargv[i+1]);
-                }
-                else if(strcmp(myargv[i], ">>")==0){
-                    app = true;
-                    myargv[i] = NULL;
-                    strcpy(append, myargv[i+1]);
-                }
-            }
-
-        //test
-            // printf(in ? "in: true\n" : "in: false\n");
-            // printf(out ? "out: true\n" : "out: false\n");
-            // printf(app ? "app: true\n" : "app: false\n");
-
-    //if input redirection(<)
-            if(in){
-                int fd0;
-                if((fd0=open(input, O_RDONLY, 0)) < 0) {
-                    perror("Could not open input file");
-                    exit(0);
-                }
-                dup2(fd0, 0);
-                close(fd0);
-            }
-
-    //if output redirection(>)
-            if(out){
-                int fd1;
-                if((fd1=creat(output, 0644)) < 0) {
-                    perror("Could not open output file");
-                    exit(0);
-                }
-                dup2(fd1, STDOUT_FILENO);
-                close(fd1);
-            }
-
-    //if append redirection(>>)
-            if(app){
-                int fd2;
-                if((fd2=open(append, O_WRONLY|O_APPEND)) < 0) {
-                    perror("Could not open file");
-                    exit(0);
-                }
-                dup2(fd2, STDOUT_FILENO);
-                close(fd2);
-            }
-
-
-            execvp(*myargv, myargv);
-            perror("error: execvp");
-            exit(1);
-
-        }
-    //if error
-        else if((id) < 0){
-            printf("fork() failed!\n");
-            exit(1);
-        }
-        else{
-            wait(0);
-        }    
-
-
-    /*===========================================================================================
-    5.Add functionality to shell to execute commands in the background. For example
-    commands like : ls -la &
-    ===========================================================================================*/
-
-    //fork code at bottom
-
-    /*===========================================================================================
-    6.Add functionality to your shell to execute the cd and pwd commands. Note these need
-    to be implemented in your shell. Use the chdir() and getpwd() functions to implement
-    these shell commands.
-    ===========================================================================================*/
-
-    //implement pwd command
-        char pwd[100];
-        if (strcmp("pwd", myargv[0]) == 0) {
-            if(getcwd(pwd, sizeof(pwd)) != NULL) {
-                printf("%s\n", pwd);
-            } else {
-                printf("pwd failed");
-            }
-            
-        }
-
-    //implement cd command
+        //implement cd command
         char *dir;
         if (strcmp(myargv[0], "cd") == 0) {
-            if(myargc > 1)
+            if (myargc > 1)
                 dir = myargv[1];
             int status = chdir(dir);
-            if(status == -1)
+            if (status == -1)
                 perror("cd failed");
+            continue;
         }
 
-
-    /*===========================================================================================
-    7.Add functionality to your shell to execute piped commands. These are commands that
-    are connected by a shell pipe, |.
-    ===========================================================================================*/
-    
-
-
-
-    //===========================================================================================
-    //BOTTOM
-    //===========================================================================================
-     
-     
-        // pid_t id;
-        // id=fork();
-
-    //parent
-        if(id>0){
-            wait(0);
-        }
-    //child
-        else if(id==0){
-            int result = execvp(myargv[0], myargv);
-        }
-    //error
-        else{
-            perror("fork error");
-        }
-    }
+        //implement pwd command
+            char pwd[100];
+            if (strcmp("pwd", myargv[0]) == 0) {
+                if (getcwd(pwd, sizeof(pwd)) != NULL) {
+                    printf("%s\n", pwd);
+                }
+                else {
+                    printf("pwd failed");
+                }
+                continue;
+            }
 
 
-    //===========================================================================================
-    //===========================================================================================
-    //MISC.
+            ////////////////////////////////////////////////////////////////////////
 
-        // //create string from user input
-        // char str_cmd[100] = "";
-        // i = 0;
-        // while(myargv[i] != NULL){
-        //     if(i>0)
-        //     strcat(str_cmd, " ");
-        //     strcat(str_cmd, myargv[i]);
-        //     i++;
-        // }
-        // strcat(str_cmd, "\0");
 
-        // //adds 'ls' functionality 
-        //     if( (strcmp(myargv[0], "ls") == 0) ) {
-        //         system(str_cmd);
-        //     }
+		    bool background = false;
+			int fd0, fd1, fd2;
+			char input[64], output[64], append[64];
+			bool in = false, out = false, app = false;
 
-    //MISC
-    //===========================================================================================
-    //===========================================================================================
 
-    return 0;
+			//determine if should run in Background
+
+			if (strcmp(myargv[myargc - 1], "&") == 0) {
+				background = true;
+				myargv[myargc - 1] = NULL;
+				myargc = myargc - 1;
+			}
+
+
+			//determine if statement is redirection
+			for (int i = 0; i < myargc; i++) {
+				if (strcmp(myargv[i], "<") == 0) {
+					in = true;
+					myargv[i] = NULL;
+					strcpy(input, myargv[i + 1]);
+				}
+				else if (strcmp(myargv[i], ">") == 0) {
+					out = true;
+					myargv[i] = NULL;
+					strcpy(output, myargv[i + 1]);
+				}
+				else if (strcmp(myargv[i], ">>") == 0) {
+					app = true;
+					myargv[i] = NULL;
+					strcpy(append, myargv[i + 1]);
+				}
+			}
+
+			//test
+				// printf(in ? "in: true\n" : "in: false\n");
+				// printf(out ? "out: true\n" : "out: false\n");
+				// printf(app ? "app: true\n" : "app: false\n");
+				// printf(background ? "background: true\n" : "background: false\n");
+
+
+        pid_t id;
+		id = fork();
+
+        //if background
+		if (background && id == 0) {
+            printf("background item");
+			//fork();
+			setpgid(0, 0);
+			execvp(*myargv, myargv);
+			perror("error: background execvp");
+			exit(1);
+		}
+
+        //child
+		if (id == 0) {
+            printf("child item");
+
+		    //if input redirection(<)
+			if (in) {
+				int fd0;
+				if ((fd0 = open(input, O_RDONLY, 0)) < 0) {
+					perror("Could not open input file");
+					exit(0);
+				}
+				dup2(fd0, 0);
+				close(fd0);
+			}
+
+			//if output redirection(>)
+			if (out) {
+				int fd1;
+				if ((fd1 = creat(output, 0644)) < 0) {
+					perror("Could not open output file");
+					exit(0);
+				}
+				dup2(fd1, STDOUT_FILENO);
+				close(fd1);
+			}
+
+			//if append redirection(>>)
+			if (app) {
+				int fd2;
+				if ((fd2 = open(append, O_WRONLY | O_APPEND)) < 0) {
+					perror("Could not open file to append");
+					exit(0);
+				}
+				dup2(fd2, STDOUT_FILENO);
+				close(fd2);
+			}
+
+			execvp(*myargv, myargv);
+			perror("error: execvp");
+			exit(1);
+			
+
+		}
+        //parent
+		else if (id>0 && !background) {
+            printf("parent item \n");
+		    wait(0);
+		}
+		//if error
+		else if ((id) < 0) {
+			printf("fork() failed!\n");
+			exit(1);
+		}
+
+	}
+	return 0;
 }
